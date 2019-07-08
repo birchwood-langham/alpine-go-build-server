7947009767cfbc4de30f78e903d3a056c7755163
Tan Quach <tan.quach@birchwoodlangham.com>
2019-07-08 06:59:00 +0100

    Updated image build.sh to properly tag the image with the Go version number

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+current_version=`docker run -t --rm birchwoodlangham/alpine-go-build-server:latest go version | cut -d ' ' -f3`
+docker rmi -f birchwoodlangham/alpine-go-build-server:${current_version}
 docker rmi -f birchwoodlangham/alpine-go-build-server:latest
 
 docker build -t birchwoodlangham/alpine-go-build-server:latest .
+next_version=`docker run -t --rm birchwoodlangham/alpine-go-build-server:latest go version | cut -d ' ' -f3`
+docker tag birchwoodlangham/alpine-go-build-server:latest birchwoodlangham/alpine-go-build-server:${next_version}
