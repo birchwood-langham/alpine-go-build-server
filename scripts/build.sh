032075809115b8dae51558c8132f8282e92059b5
Tan Quach <tan.quach@birchwoodlangham.com>
2019-08-12 09:46:51 +0100

    Updated build script to support GitHub and public repos

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,58 +13,64 @@ if [[ -z ${CREATE_TARBALL} ]]; then
     CREATE_TARBALL=false
 fi
 
-if [ -z $TOKEN ]; then
-    echo "TOKEN has not been set, cannot continue"
-    exit 1
-fi
-
 if [[ -z ${PROJECT_PATH} ]] || [[ -z ${OUTPUT_PATH} ]] || [[ -z ${MAIN_PATH} ]]; then
     echo 'PROJECT_PATH, OUTPUT_PATH, MAIN_PATH must be defined'
     exit 1
+fi
+
+if [[ -z ${USE_GITLAB} ]]; then
+    if [[ -n ${TOKEN} ]]; then
+        echo 'Setting redirect for private GitHub repo'
+        git config --global url."https://$TOKEN@github.com".insteadOf "https://github.com"
+    fi
+    
+    PROJECT_URL=https://github.com/${PROJECT_PATH}
 else
-
-    git config --global url."https://oauth2:$TOKEN@gitlab.com".insteadOf "https://gitlab.com"
-
-    if [[ ${USE_DEP} = true ]]; then 
-        cd $GOPATH/src/gitlab.com/${PROJECT_PATH}
-    else 
-        cd /build
+    if [[ -n ${TOKEN} ]]; then 
+        echo 'Setting redirect for private GitLab repo'
+        git config --global url."https://oauth2:$TOKEN@gitlab.com".insteadOf "https://gitlab.com"
     fi
 
     PROJECT_URL=https://gitlab.com/${PROJECT_PATH}
-    PROJECT=`echo ${PROJECT_URL##*/} | cut -d '.' -f 1`
+fi
 
-    echo Cloning project from ${PROJECT_URL}
+if [[ ${USE_DEP} = true ]]; then 
+    cd $GOPATH/src/gitlab.com/${PROJECT_PATH}
+else 
+    cd /build
+fi
 
-    if [[ -z ${BRANCH} ]]; then
-        git clone ${PROJECT_URL}
-    else
-        git clone --single-branch --branch ${BRANCH} ${PROJECT_URL}
-    fi
+PROJECT=`echo ${PROJECT_URL##*/} | cut -d '.' -f 1`
 
-    cd $PROJECT
+echo Cloning project from ${PROJECT_URL}
 
-    if [[ ! -z ${TAG} ]]; then 
-        echo Checking out tag: ${TAG}
-        git checkout tags/${TAG}
-    fi
+if [[ -z ${BRANCH} ]]; then
+    git clone ${PROJECT_URL}
+else
+    git clone --single-branch --branch ${BRANCH} ${PROJECT_URL}
+fi
 
-    if [[ ${USE_DEP} = true ]]; then 
-        dep ensure
-    else
-        go get
-    fi
+cd $PROJECT
 
-    ## we compile and force recompile dependencies without C dependencies
-    go build -a -installsuffix cgo -o ${OUTPUT_PATH} ${MAIN_PATH}
+if [[ ! -z ${TAG} ]]; then 
+    echo Checking out tag: ${TAG}
+    git checkout tags/${TAG}
+fi
 
-    if [[ $CREATE_TARBALL = true ]]; then
-        rm -rf .git
+if [[ ${USE_DEP} = true ]]; then 
+    dep ensure
+else
+    go get
+fi
 
-        cd ..
-        tar -czf "${PROJECT}.tar.gz" ${PROJECT}
-        cp "${PROJECT}.tar.gz" $(dirname ${OUTPUT_PATH})/
-    fi
-    
+## we compile and force recompile dependencies without C dependencies
+go build -a -installsuffix cgo -o ${OUTPUT_PATH} ${MAIN_PATH}
+
+if [[ $CREATE_TARBALL = true ]]; then
+    rm -rf .git
+
+    cd ..
+    tar -czf "${PROJECT}.tar.gz" ${PROJECT}
+    cp "${PROJECT}.tar.gz" $(dirname ${OUTPUT_PATH})/
 fi
 
